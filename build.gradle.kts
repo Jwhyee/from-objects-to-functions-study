6ec26688e2528b433634396577a4d0ca2e6fd428
Jwhyee <kawnsdud@naver.com>
2025-02-04 22:57:00 +0900

    add : 필요한 의존 정보 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,38 @@ repositories {
     mavenCentral()
 }
 
+val http4kVersion: String by project
+val junitBomVersion: String by project
+val pesticideVersion: String by project
+val striktVersion: String by project
+
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    implementation("org.http4k:http4k-core:${http4kVersion}")
+    implementation("org.http4k:http4k-server-jetty:${http4kVersion}")
+
+    testImplementation(platform("org.junit:junit-bom:${junitBomVersion}"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.http4k:http4k-client-jetty:${http4kVersion}")
+    testImplementation("com.ubertob.pesticide:pesticide-core:${pesticideVersion}")
+    testImplementation("io.strikt:strikt-core:${striktVersion}")
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    //if true show println in test console
+    testLogging.showStandardStreams = false
+
+    // start tests every time, even when code not changed
+    outputs.upToDateWhen { false }
 }
 kotlin {
     jvmToolchain(21)
