009f8eaeff37186af9106d5817165fbb1a6c6175
Jwhyee <kawnsdud@naver.com>
2025-02-06 21:46:42 +0900

    add : 웹 서버 구성 후 웰컴 페이지 추가

--- src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.project
-
-fun main() {
-    println("Hello World!")
-}
---
+++ src/main/kotlin/jettai/Main.kt
@@ -0,0 +1,22 @@
+package jettai
+
+import org.http4k.core.HttpHandler
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+// language=HTML
+val htmlPage = """
+    <html>
+        <body>
+            <h1 style="text-align: center; font-size: 3em">Hello Functional World!</h1>
+        </body>
+    </html>
+""".trimIndent()
+
+val handler: HttpHandler = { Response(Status.OK).body(htmlPage) }
+
+fun main() {
+    handler.asServer(Jetty(8080)).start()
+}
