70ae3d555c6d7ff415b529acaa1b1f2b0d3854e9
Jwhyee <kawnsdud@naver.com>
2025-02-09 22:24:20 +0900

    refactor : 메인 함수 수정

--- src/main/kotlin/jettai/Main.kt
+++ src/main/kotlin/jettai/Main.kt
@@ -1,8 +1,19 @@
 package jettai
 
+import jettai.entity.ListName
+import jettai.entity.ToDoItem
+import jettai.entity.ToDoList
+import jettai.entity.User
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
 fun main() {
-    Zettai().asServer(Jetty(8080)).start()
+    val user = "frank"
+    val listName = "shipping"
+    val items = listOf("carrots", "apples", "milk")
+    val toDoList = ToDoList(
+        ListName(listName), items.map(::ToDoItem)
+    )
+    val lists = mapOf(User(user) to listOf(toDoList))
+    Zettai(lists).asServer(Jetty(8080)).start()
 }
