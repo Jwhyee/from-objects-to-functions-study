60b78b93ba910c6fd4578f1d6a8df4514b38e5e5
Jwhyee <kawnsdud@naver.com>
2025-02-12 22:41:35 +0900

    refactor : RpnCalc 계산기 리팩터링

--- src/test/kotlin/exercise/chapter02/RpnCalc.kt
+++ src/test/kotlin/exercise/chapter02/RpnCalc.kt
@@ -3,31 +3,31 @@ package exercise.chapter02
 import java.util.*
 
 object RpnCalc {
+    private val operationsMap = mapOf<String, (Double, Double) -> Double>(
+        "+" to Double::plus,
+        "-" to Double::minus,
+        "/" to Double::div,
+        "*" to Double::times
+    )
+
+    fun calc(expression: String): Double {
+        val funStack = FunStack<Double>()
+        return StringTokenizer(expression).toList().map { "$it" }
+            .fold(funStack, ::reduce)
+            .pop().first
+    }
+
     /**
      * RPN은 연산자가 피연산자 뒤에 오는 수학 표기법
      * 연산자를 두 피연산자의 중간에 배치한다.
      * 우선 순위를 표기하기 위해 괄호 대신 피연산자 뒤에 연산자를 배치한다.
      * */
-    fun calc(expression: String): Double {
-        val result = StringTokenizer(expression)
-            .toList()
-            .fold(FunStack<Double>()) { acc, v ->
-                val funStack = "$v".toDoubleOrNull()?.let { d ->
-                    acc.push(d)
-                } ?: run {
-                    val (v1, stack1) = acc.pop()
-                    val (v2, stack2) = stack1.pop()
-                    val result = when(v) {
-                        "+" -> { v2 + v1 }
-                        "-" -> { v2 - v1 }
-                        "/" -> { v2 / v1 }
-                        "*" -> { v2 * v1 }
-                        else -> throw IllegalArgumentException("Invalid Operation")
-                    }
-                    stack2.push(result)
-                }
-                funStack
-            }
-        return result.pop().first
-    }
+    private fun reduce(stack: FunStack<Double>, token: String): FunStack<Double> =
+        if(operationsMap.containsKey(token)) {
+            val (b, prevStack) = stack.pop()
+            val (a, newStack) = prevStack.pop()
+            newStack.push(operationsMap[token]!!(a, b))
+        } else {
+            stack.push(token.toDouble())
+        }
 }
